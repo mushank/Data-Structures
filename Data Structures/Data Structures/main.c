cb0b802a36a3f0f356a09ab42fa3aae7839941ba
Jack <mushank@Gmail.com>
2016-09-25 00:17:22 +0800

    Initial Project

---
+++ Data Structures/Data Structures/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Data Structures
+//
+//  Created by Jack on 9/25/16.
+//  Copyright © 2016 mushank. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
